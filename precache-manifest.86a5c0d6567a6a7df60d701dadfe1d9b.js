6b781d95ab2420d39ecadce152650f65a0e42e87
Karen Sarksyan <nichollascarter@gmail.com>
2021-03-30 23:27:50 +0200

    Updates

---
+++ precache-manifest.86a5c0d6567a6a7df60d701dadfe1d9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897f0da8bd27795e4bc18a845c50287a",
+    "url": "/subjx-mapper/index.html"
+  },
+  {
+    "revision": "74d317cdcd53d8957364",
+    "url": "/subjx-mapper/static/css/main.efa28815.chunk.css"
+  },
+  {
+    "revision": "3024bf8ffa98f74b4849",
+    "url": "/subjx-mapper/static/js/2.ca22a8bd.chunk.js"
+  },
+  {
+    "revision": "74d317cdcd53d8957364",
+    "url": "/subjx-mapper/static/js/main.e7d4a6ef.chunk.js"
+  },
+  {
+    "revision": "c6138a6d06bbfd249914",
+    "url": "/subjx-mapper/static/js/runtime-main.6066b9cc.js"
+  }
+]);
